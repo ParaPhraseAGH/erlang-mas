5c3e8fe1fc8bc9163928d56a7fbf91c08873c412
Marcin Miszczyk <mpmiszczyk@gmail.com>
2014-11-10 14:15:31 +0100

    Use Proplist in fawor of dict.

--- src/utils/mas_config.erl
+++ src/utils/mas_config.erl
@@ -5,20 +5,20 @@
 
 -export([proplist_to_record/1]).
 
--define(LOAD(Prop, Dict), Prop = dict:fetch(Prop,Dict)).
+-define(LOAD(Prop, Proplist),
+        Prop = proplists:get_value(Prop,Proplist)).
 
 %% @doc Transform a proplist with config properties to a record
 -spec proplist_to_record([tuple()]) -> config().
 proplist_to_record(Proplist) ->
-    Dict = dict:from_list(Proplist),
-    #config{?LOAD(agent_env, Dict),
-            ?LOAD(topology, Dict),
-            ?LOAD(migration_probability, Dict),
-            ?LOAD(log_dir, Dict),
-            ?LOAD(islands, Dict),
-            ?LOAD(population_size, Dict),
-            ?LOAD(write_interval, Dict),
-            ?LOAD(skel_workers, Dict),
-            ?LOAD(arena_timeout, Dict),
-            ?LOAD(skel_split_size, Dict),
-            ?LOAD(skel_pull, Dict)}.
+    #config{?LOAD(agent_env, Proplist),
+            ?LOAD(topology, Proplist),
+            ?LOAD(migration_probability, Proplist),
+            ?LOAD(log_dir, Proplist),
+            ?LOAD(islands, Proplist),
+            ?LOAD(population_size, Proplist),
+            ?LOAD(write_interval, Proplist),
+            ?LOAD(skel_workers, Proplist),
+            ?LOAD(arena_timeout, Proplist),
+            ?LOAD(skel_split_size, Proplist),
+            ?LOAD(skel_pull, Proplist)}.
